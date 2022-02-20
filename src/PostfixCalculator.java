e8c47c6620ad5ed3283a4954ec99d82a0f9c28c1
Zende-Magloire <89619545+Zende-Magloire@users.noreply.github.com>
2022-02-19 22:57:21 -0500

    ..

--- src/PostfixCalculator.java
+++ src/PostfixCalculator.java
@@ -1,4 +1,4 @@
-
+import java.util.StringTokenizer;
 
 public class PostfixCalculator
 {
@@ -11,29 +11,39 @@ public class PostfixCalculator
         // Algorithm to convert infix string to postfix string
         //
         // create an empty string for postfix expression
+        String postfix = new String();
         // create a stack of strings
+        Stack<String> mStack = new Stack<>();
         // tokenize the infix expression with a delimiter of space
-        //
+        StringTokenizer tokenizer = new StringTokenizer(infix, " ");
         // while tokenizer has more tokens
-        //   token = get next token from tokenizer
-        //
-        //   if token is an open parenthesis
-        //     push token on stack
-        //
-        //   else if token is a close parenthesis
-        //     operator = pop top from stack
-        //     while operator doesn't equal open parenthesis
-        //       append operator and a single space to postfix expression
-        //       operator = pop top from stack
-        //
-        //   else if token is an operator
-        //     while stack is not empty and precedence of token is less than top of stack (peek)
-        //       pop top from stack and append it and single space to postfix expression
-        //     after loop, push operator token onto stack
-        //
-        //   else
-        //     append token and single space to postfix expression
-        //
+        while (tokenizer.hasMoreTokens())
+        {
+            //   token = get next token from tokenizer
+            String token = tokenizer.nextToken();
+            //   if token is an open parenthesis
+            if (token.equals("("))
+            //     push token on stack
+            mStack.push(token);
+            //   else if token is a close parenthesis
+            else if (token.equals(")"));
+            //     operator = pop top from stack
+            String operator = mStack.pop();
+            //     while operator doesn't equal open parenthesis
+            while (!operator.equals("("))
+            {
+            //   append operator and a single space to postfix expression
+            //   operator = pop top from stack
+            //
+            //   else if token is an operator
+            //     while stack is not empty and precedence of token is less than top of stack (peek)
+            //       pop top from stack and append it and single space to postfix expression
+            //     after loop, push operator token onto stack
+            //
+            //   else
+            //     append token and single space to postfix expression
+            }
+        }
         // while stack is not empty
         //   pop and append operator and single space to postfix expression 
         //
